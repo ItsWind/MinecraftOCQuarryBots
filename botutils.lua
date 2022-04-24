d26ccf5b9a78b9512b1a5c486e8fd91f643ae805
Windwhistle <stricklandjake63@gmail.com>
2022-04-24 03:49:39 -0500

    Initial commit

---
+++ botutils.lua
@@ -0,0 +1,109 @@
+local eve = {}
+local computer = require("computer")
+local robot = require("robot")
+local component = require("component")
+
+local configVars = {
+  ["listenPort"] = ,
+  ["lowPowerPercent"] = 0.1,
+  ["magicNumber"] = 20,
+  ["posStorage"] = {
+    ["x"] = 5.5,
+    ["y"] = 51.5,
+    ["z"] = 14.5
+  },
+  ["posCharging"] = {
+    ["x"] = -1.5,
+    ["y"] = 54.5,
+    ["z"] = 16.5
+  }
+}
+function eve.getConfig(str)
+  return configVars[str]
+end
+
+function eve.savedPosValid(posT)
+  if posT ~= nil then
+    if posT["x"] ~= nil and posT["y"] ~= nil and posT["z"] ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
+local turnToUse = { [true] = robot.turnLeft, [false] = robot.turnRight }
+function eve.quarryTurn(useLeft)
+  turnToUse[useLeft]()
+end
+
+local facingNums = {
+  ["n"] = 2,
+  ["e"] = 5,
+  ["s"] = 3,
+  ["w"] = 4
+}
+function eve.faceNum(facingNum)
+  while component.navigation.getFacing() ~= facingNum do robot.turnRight() end
+end
+function eve.face(facingStr)
+  eve.faceNum(facingNums[facingStr])
+end
+
+function eve.multipleMove(moveFunc, times, doBreak, breakFunc)
+  doBreak = doBreak or false
+  breakFunc = breakFunc or robot.swing
+  local amountMoved = 0
+  repeat
+    if doBreak then breakFunc() end
+    if moveFunc() then amountMoved = amountMoved+1 end
+  until(amountMoved >= times)
+end
+
+local function moveXZ(facingNumNeeded)
+  if component.navigation.getFacing() ~= facingNumNeeded then
+    robot.turnRight()
+  else
+    robot.swing()
+    robot.forward()
+  end
+end
+local function moveY(up)
+  if up then
+    robot.swingUp()
+    robot.up()
+  else
+    robot.swingDown()
+    robot.down()
+  end
+end
+function eve.gotoPoint(posT, yFirst)
+  if eve.savedPosValid(posT) then
+    local currPosX, currPosY, currPosZ = component.navigation.getPosition()
+    local relX, relY, relZ = posT["x"]-currPosX, posT["y"]-currPosY, posT["z"]-currPosZ
+    if relY ~= 0 and yFirst then moveY(relY>0)
+    elseif relX > 0 then moveXZ(5)
+    elseif relX < 0 then moveXZ(4)
+    elseif relZ > 0 then moveXZ(3)
+    elseif relZ < 0 then moveXZ(2)
+    elseif relY ~= 0 and not yFirst then moveY(relY>0)
+    else
+      if posT["facingNum"] ~= nil then eve.faceNum(posT["facingNum"]) end
+      return true
+    end
+  else
+    return nil
+  end
+  return false
+end
+
+function eve.say(str)
+ os.execute("clear")
+  print(str)
+end
+
+function eve.getPowerPercent()
+  local percent = computer.energy() / computer.maxEnergy()
+  return percent
+end
+
+return eve
---
+++ listenpls.lua
@@ -0,0 +1,174 @@
+local eve = require("botutils")
+local robot = require("robot")
+local computer = require("computer")
+local event = require("event")
+local component = require("component")
+
+local invController = component.inventory_controller
+local geolyzer = component.geolyzer
+
+local keyToPerform = "stop"
+
+local savedPositions = {
+  ["storage"] = eve.getConfig("posStorage"),
+  ["charging"] = eve.getConfig("posCharging")
+}
+
+local networkPass = "t?aeWwZd36_A6[Yf"
+event.listen("modem_message", function(_, _, from, port, _, message)
+  local colonIndex = message:find(":")
+  if port == eve.getConfig("listenPort") and colonIndex ~= nil and message:sub(1, colonIndex-1) == networkPass then
+    keyToPerform = message:sub(colonIndex+1)
+    eve.say("I'm performing " .. keyToPerform)
+  end
+end )
+
+local function checkLowPower(percentMod)
+  percentMod = percentMod or 0
+  if eve.getPowerPercent() <= eve.getConfig("lowPowerPercent")+percentMod then
+    keyToPerform = "gotocharging"
+    eve.say("LOW POWER: Automatically going to charging station.")
+    return true
+  end
+  return false
+end
+
+local function checkInvFull(maxSlot)
+  maxSlot = maxSlot or robot.inventorySize()
+  if invController.getStackInInternalSlot(maxSlot) ~= nil then
+    keyToPerform = "gotostorage"
+    eve.say("INV FULL: Automatically storing in storage chest.")
+    return true
+  end
+  return false
+end
+
+local validFuncs = {
+  ["stop"] = function() os.sleep(5) end,
+  ["unloadall"] = function()
+    for i=3,robot.inventorySize() do
+      robot.select(i)
+      robot.dropUp()
+    end
+    robot.select(1)
+    eve.multipleMove(robot.down, 1)
+    if not checkLowPower(eve.getConfig("lowPowerPercent")) then
+      if eve.savedPosValid(savedPositions["quarry"]) then
+        keyToPerform = "gotoquarry"
+      else
+        keyToPerform = "stop"
+      end
+    end
+  end,
+  ["gotostorage"] = function()
+    local x, y, z = component.navigation.getPosition()
+    if savedPositions["quarry"] ~= nil and savedPositions["quarry"]["y"] == nil then savedPositions["quarry"]["y"] = y end
+    while keyToPerform == "gotostorage" do
+      if eve.gotoPoint(savedPositions["storage"], false) then
+        keyToPerform = "unloadall"
+      end
+      os.sleep(0)
+    end
+  end,
+  ["gotocharging"] = function()
+    local x, y, z = component.navigation.getPosition()
+    if savedPositions["quarry"] ~= nil and savedPositions["quarry"]["y"] == nil then savedPositions["quarry"]["y"] = y end
+    while keyToPerform == "gotocharging" do
+      if eve.gotoPoint(savedPositions["charging"], false) then
+        if eve.getPowerPercent() >= 0.995 then
+          eve.multipleMove(robot.down, 4)
+          if not checkInvFull(math.floor(robot.inventorySize()/2)) then
+            if eve.savedPosValid(savedPositions["quarry"]) then
+              keyToPerform = "gotoquarry"
+            else
+              keyToPerform = "stop"
+            end
+          end
+        end
+        os.sleep(5)
+      end
+      os.sleep(0)
+    end
+  end,
+  ["gotoquarry"] = function()
+    if eve.savedPosValid(savedPositions["quarry"]) then
+      while keyToPerform == "gotoquarry" do
+        if eve.gotoPoint(savedPositions["quarry"], true) then
+          keyToPerform = "minequarry"
+          eve.say("I'm performing minequarry")
+        end
+        os.sleep(0)
+      end
+    else
+      keyToPerform = "stop"
+      eve.say("No saved quarry location found.")
+    end
+  end,
+  ["minequarry"] = function()
+    local startPosX, startPosY, startPosZ = component.navigation.getPosition()
+    savedPositions["quarry"] = { ["x"] = startPosX, ["z"] = startPosZ, ["facingNum"] = component.navigation.getFacing() }
+    
+    local magicNumber = eve.getConfig("magicNumber")
+    local useLeft = true
+    local rowsMined = 0
+    while keyToPerform == "minequarry" do
+      local numMined = 0
+      repeat
+        if checkInvFull() or checkLowPower() then os.sleep(0) break end
+        local blockBelow = geolyzer.analyze(0)
+        local blockBelowName = blockBelow["name"]
+        local blockBelowIsLiquid = blockBelow["harvestLevel"] == -1
+        if blockBelowName == "minecraft:air" then
+          robot.placeDown()
+        elseif blockBelowName == "minecraft:dirt" or blockBelowName == "minecraft:gravel" then
+          robot.swingDown()
+          robot.placeDown()
+        elseif blockBelowIsLiquid then
+          robot.select(2)
+          robot.placeDown()
+          robot.swingDown()
+          robot.select(1)
+          robot.placeDown()
+        end
+        robot.swingDown()
+        numMined = numMined+1
+        if numMined < magicNumber then
+          eve.multipleMove(robot.forward, 3, true)
+        end
+        os.sleep(0)
+      until(numMined >= magicNumber)
+      rowsMined = rowsMined+1
+      eve.quarryTurn(useLeft)
+      if rowsMined < magicNumber then
+        eve.multipleMove(robot.forward, 3, true)
+        eve.quarryTurn(useLeft)
+        useLeft = not useLeft
+      else
+        local currX, currY, currZ = component.navigation.getPosition()
+        if currY > 6 then
+          eve.quarryTurn(useLeft)
+          eve.multipleMove(robot.down, 1)
+          rowsMined = 0
+        else
+          savedPositions["quarry"] = nil
+          keyToPerform = "gotocharging"
+          eve.say("Y5 REACHED: Going back to charging station.")
+        end
+      end
+    end
+  end
+}
+
+local function perform()
+  eve.say("I'm listening..")
+  computer.beep()
+  
+  component.modem.open(eve.getConfig("listenPort"))
+  
+  while keyToPerform ~= "completestop" do
+    validFuncs[keyToPerform]()
+    os.sleep(0)
+  end
+end
+
+perform()
